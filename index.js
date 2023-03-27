04f7f2e0ae8f8e4af59d28f2875f4893cf34e2aa
KittisakNdpi <kittisak.ndpi@outlook.com>
2023-03-27 12:24:55 +0700

    commit

--- index.js
+++ index.js
@@ -1,10 +1,81 @@
-const express = require('express')
-const app = express()
+const mysql = require('mysql2');
+const express = require('express');
+const bodyParser = require('body-parser');
+const short = require('short-uuid');
+const app = express();
+const port = 3000;
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'sdcs'
+});
+app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+//regis student
+app.post('/student/register', function (req, res) {
+  con.query(`INSERT INTO sdcs.tab_student
+            (id, username, password, code, name, lastname, branchId, email, createAt, updateAt)
+            VALUES(
+              '${short.generate()}', 
+              '${req.body.username}', 
+              '${req.body.password}', 
+              '${req.body.code}', 
+              '${req.body.name}', 
+              '${req.body.lastname}', 
+              '${req.body.branchId}', 
+              '${req.body.email}',
+              CURRENT_TIMESTAMP(), 
+              CURRENT_TIMESTAMP()
+            );
+  `, (err, result) => {
+    if (err) {
+      res.status(400).json({
+        code: err.code,
+        message: err.message
+      })
+    } else {
+      res.status(200).json({
+        "status": "sucess",
+        "data": result
+      }).send()
+    }
+  })
 })
 
-app.listen(3000)
+//update student
+app.post('/student/update', (req, res) => {
+  con.query(``, (err, result) => {
+    if (err) {
+      res.status(400).json({
+        code: err.code,
+        message: err.message
+      })
+    } else {
+      res.status(200).json({
+        "status": "sucess",
+        "data": result
+      }).send()
+    }
+  })
+})
+
+//delete student
+app.delete('/student/update/', (req, res) => {
+  con.query(``, (err, result) => {
+    if (err) {
+      res.status(400).json({
+        code: err.code,
+        message: err.message
+      })
+    } else {
+      res.status(200).json({
+        "status": "sucess",
+        "data": result
+      }).send()
+    }
+  })
+})
+
+app.listen(port, () => { console.log(" start index.js on port", port) });
 
 
